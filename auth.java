e4f038b428218c1f16abf2b49e86f4d8fd60afd0
Chirag <chiragrrai44@gmail.com>
2026-02-13 16:39:18 +0530

    working on password authentication

---
+++ auth.java
@@ -0,0 +1,11 @@
+import java.net.PasswordAuthentication;
+public class auth
+{
+    public void demo()
+    {
+        String username="Chirag";
+        char password[]={'C','h','i','r','a','g','r','r','a','i','8','1','0','5'};
+        PasswordAuthentication p =new PasswordAuthentication(username, password);
+        System.out.println(p.getUserName());
+    }
+}
