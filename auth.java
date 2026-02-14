d423718b903c8c2ebf0314b1c28618278f0c36dc
Chirag <chiragrrai44@gmail.com>
2026-02-14 22:08:00 +0530

    deleted classes

--- auth.java
+++ auth.java
@@ -7,5 +7,8 @@ public class auth
         char password[]={'C','h','i','r','a','g','r','r','a','i','8','1','0','5'};
         PasswordAuthentication p =new PasswordAuthentication(username, password);
         System.out.println(p.getUserName());
+        System.out.println(p.getPassword());
+        System.out.println(String.copyValueOf(p.getPassword()));
     }
+    
 }
